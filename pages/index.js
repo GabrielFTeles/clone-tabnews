781a8136a1b45731c47a165e81f426dc2ec21883
Gabriel Teles <gabrielfariateles@gmail.com>
2026-01-20 21:43:11 -0300

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function Home() {
+    return (
+        <h1>
+            Carl Sagan: If you wish to make an apple pie from scratch, you must
+            first invent the universe.
+        </h1>
+    );
+}
+
+export default Home;
