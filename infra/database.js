169bf88b178fb1a8d0bfcc919434bd2903bdab75
Gabriel Teles <gabrielfariateles@gmail.com>
2026-01-25 21:19:23 -0300

    move `client.connect()` at `database.js` outside try/catch

--- infra/database.js
+++ infra/database.js
@@ -8,9 +8,9 @@ async function query(queryObject) {
         database: process.env.POSTGRES_DB,
         password: process.env.POSTGRES_PASSWORD,
     });
+    await client.connect();
 
     try {
-        await client.connect();
         const result = await client.query(queryObject);
         return result;
     } catch (error) {
