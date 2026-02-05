841c0cce24c5802075fb810766eef13ad7d2d9a5
Gabriel Teles <gabrielfariateles@gmail.com>
2026-02-05 12:53:52 -0300

    feat: create `migrator` model

---
+++ models/migrator.js
@@ -0,0 +1,53 @@
+import { resolve } from "node:path";
+import migrationRunner from "node-pg-migrate";
+import database from "infra/database.js";
+
+const defaultMigrationOptions = {
+    dryRun: true,
+    dir: resolve("infra", "migrations"),
+    direction: "up",
+    verbose: true,
+    migrationsTable: "pgmigrations",
+};
+
+async function listPendingMigrations() {
+    let dbClient;
+
+    try {
+        dbClient = await database.getNewClient();
+
+        const pendingMigrations = await migrationRunner({
+            ...defaultMigrationOptions,
+            dbClient,
+        });
+
+        return pendingMigrations;
+    } finally {
+        await dbClient?.end();
+    }
+}
+
+async function runPendingMigrations() {
+    let dbClient;
+
+    try {
+        dbClient = await database.getNewClient();
+
+        const migratedMigrations = await migrationRunner({
+            ...defaultMigrationOptions,
+            dbClient,
+            dryRun: false,
+        });
+
+        return migratedMigrations;
+    } finally {
+        await dbClient?.end();
+    }
+}
+
+const migrator = {
+    listPendingMigrations,
+    runPendingMigrations,
+};
+
+export default migrator;
