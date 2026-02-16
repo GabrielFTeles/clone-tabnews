8b045a1bdd0c63d681b7231471fea775c587c788
Gabriel Teles <gabrielfariateles@gmail.com>
2026-02-16 20:07:55 -0300

    feat: send activation email after `user` registration

---
+++ infra/migrations/1771281444503_create-user-activation-tokens.js
@@ -0,0 +1,32 @@
+exports.up = (pgm) => {
+    pgm.createTable("user_activation_tokens", {
+        id: {
+            type: "uuid",
+            primaryKey: true,
+            default: pgm.func("gen_random_uuid()"),
+        },
+
+        used_at: {
+            type: "timestamptz",
+            notNull: false,
+        },
+
+        user_id: {
+            type: "uuid",
+        },
+
+        expires_at: {
+            type: "timestamptz",
+            notNull: true,
+            default: pgm.func("timezone('utc', now())"),
+        },
+
+        updated_at: {
+            type: "timestamptz",
+            notNull: true,
+            default: pgm.func("timezone('utc', now())"),
+        },
+    });
+};
+
+exports.down = false;
