ab11df4a112be52c9010bd79454b6f08af81a375
Gabriel Teles <gabrielfariateles@gmail.com>
2026-02-23 20:13:32 -0300

    feat: allow `update:user:others` to update other users

--- tests/integration/api/v1/users/[username]/patch.test.js
+++ tests/integration/api/v1/users/[username]/patch.test.js
@@ -127,7 +127,7 @@ describe("PATCH /api/v1/users/[username]", () => {
             );
 
             const response = await fetch(
-                "http://localhost:3000/api/v1/users/user1",
+                "http://localhost:3000/api/v1/users/userA",
                 {
                     method: "PATCH",
                     headers: {
@@ -342,6 +342,60 @@ describe("PATCH /api/v1/users/[username]", () => {
 
             expect(correctPasswordMatch).toBe(true);
             expect(incorrectPasswordMatch).toBe(false);
+        });
+    });
+
+    describe("Privileged user", () => {
+        test("With 'update:user:others' targeting 'defaultUser'", async () => {
+            const privilegedUser = await orchestrator.createUser();
+            const activatedPrivilegeduser =
+                await orchestrator.activateUser(privilegedUser);
+
+            await orchestrator.addFeaturesToUser(privilegedUser, [
+                "update:user:others",
+            ]);
+
+            const privilegedUserSession = await orchestrator.createSession(
+                activatedPrivilegeduser.id,
+            );
+
+            const defaultUser = await orchestrator.createUser();
+
+            const response = await fetch(
+                `http://localhost:3000/api/v1/users/${defaultUser.username}`,
+                {
+                    method: "PATCH",
+                    headers: {
+                        Cookie: `session_id=${privilegedUserSession.token}`,
+                        "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({
+                        username: "AlteradoPorPrivilegiado",
+                    }),
+                },
+            );
+
+            expect(response.status).toBe(200);
+
+            const responseBody = await response.json();
+
+            expect(responseBody).toEqual({
+                id: defaultUser.id,
+                username: "AlteradoPorPrivilegiado",
+                email: defaultUser.email,
+                password: responseBody.password,
+                features: defaultUser.features,
+                created_at: responseBody.created_at,
+                updated_at: responseBody.updated_at,
+            });
+
+            expect(uuidVersion(responseBody.id)).toBe(4);
+            expect(Date.parse(responseBody.created_at)).not.toBeNaN();
+            expect(Date.parse(responseBody.updated_at)).not.toBeNaN();
+
+            expect(responseBody.updated_at > responseBody.created_at).toBe(
+                true,
+            );
         });
     });
 });
--- tests/orchestrator.js
+++ tests/orchestrator.js
@@ -99,6 +99,11 @@ function extractUUID(text) {
     return match ? match[0] : null;
 }
 
+async function addFeaturesToUser(userObject, features) {
+    const updatedUser = await user.addFeatures(userObject.id, features);
+    return updatedUser;
+}
+
 const orchestrator = {
     waitForAllServices,
     clearDatabase,
@@ -109,6 +114,7 @@ const orchestrator = {
     deleteAllEmails,
     getLastEmail,
     extractUUID,
+    addFeaturesToUser,
 };
 
 export default orchestrator;
